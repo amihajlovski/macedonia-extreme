a7c77b22591b2cfd5be5f1f0f4cce470617d6a46
Aleksandar <a.mihajlovski91@gmail.com>
2015-05-09 16:23:00 +0200

    Changed home page icons

--- web/app/controllers/HomeController.js
+++ web/app/controllers/HomeController.js
@@ -5,9 +5,9 @@
  */
 
 function HomeController($window, $scope){
-    document.getElementById('logo').style.height = $window.innerHeight*2 + 'px';
-    document.getElementById('row1').style.height = ($window.innerHeight) + 'px';
-    document.getElementById('row2').style.height = ($window.innerHeight) + 'px';
+    document.getElementById('logo').style.height = $window.innerHeight + 'px';
+    document.getElementById('row1').style.height = ($window.innerHeight / 2) + 'px';
+    document.getElementById('row2').style.height = ($window.innerHeight / 2) + 'px';
     $scope.title = "M A C E D O N I A          X         B I K I N G   T O U R S";
     
 };
